91277a79d2f1872c1e43bfb90901e6bec9d0ab3b
Adrian <adriantutuianu01@gmail.com>
2022-08-24 00:26:32 +0100

    Compound Assignment With Augmented Subtraction

--- src/Text.js
+++ src/Text.js
@@ -1,22 +1,16 @@
-// In programming, it is common to use assignments to modify the contents of a variable. Remember that everything to the right of the equals sign is evaluated first, so we can say:
+// Like the += operator, -= subtracts a number from a variable.
 
-// myVar = myVar + 5;
-// to add 5 to myVar. Since this is such a common pattern, there are operators which do both a mathematical operation and assignment in one step.
+// // myVar = myVar - 5;
+// will subtract 5 from myVar. This can be rewritten as:
 
-// One such operator is the += operator.
+// myVar -= 5;
+// Convert the assignments for a, b, and c to use the -= operator.
 
-// let myVar = 1;
-// myVar += 5;
-// console.log(myVar);
-// 6 would be displayed in the console.
-
-// Convert the assignments for a, b, and c to use the += operator.
-
-let a = 3;
-let b = 17;
-let c = 12;
+let a = 11;
+let b = 9;
+let c = 3;
 
 // Only change code below this line
-a += 12;
-b += 9;
-c += 7;
+a -= 6;
+b -= 15;
+c -= 1;
