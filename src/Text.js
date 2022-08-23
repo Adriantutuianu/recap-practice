c0750e13a1119712d62d4c6df4c6338cd71adcf9
Adrian <adriantutuianu01@gmail.com>
2022-08-24 00:27:32 +0100

    Compound Assignment With Augmented Multiplication

--- src/Text.js
+++ src/Text.js
@@ -1,16 +1,16 @@
-// Like the += operator, -= subtracts a number from a variable.
+// //The *= operator multiplies a variable by a number.
 
-// // myVar = myVar - 5;
-// will subtract 5 from myVar. This can be rewritten as:
+// myVar = myVar * 5;
+// will multiply myVar by 5. This can be rewritten as:
 
-// myVar -= 5;
-// Convert the assignments for a, b, and c to use the -= operator.
+// myVar *= 5;
+// Convert the assignments for a, b, and c to use the *= operator.
 
-let a = 11;
-let b = 9;
-let c = 3;
+let a = 5;
+let b = 12;
+let c = 4.6;
 
 // Only change code below this line
-a -= 6;
-b -= 15;
-c -= 1;
+a *= 5;
+b *= 3;
+c *= 10;
