d554a6eec81a21c25c452be41998ea406174a1c6
Adrian <adriantutuianu01@gmail.com>
2022-08-24 00:28:14 +0100

    Compound Assignment With Augmented Division

--- src/Text.js
+++ src/Text.js
@@ -1,16 +1,16 @@
-// //The *= operator multiplies a variable by a number.
+// The /= operator divides a variable by another number.
 
-// myVar = myVar * 5;
-// will multiply myVar by 5. This can be rewritten as:
+// myVar = myVar / 5;
+// Will divide myVar by 5. This can be rewritten as:
 
-// myVar *= 5;
-// Convert the assignments for a, b, and c to use the *= operator.
+// myVar /= 5;
+// Convert the assignments for a, b, and c to use the /= operator.
 
-let a = 5;
-let b = 12;
-let c = 4.6;
+let a = 48;
+let b = 108;
+let c = 33;
 
 // Only change code below this line
-a *= 5;
-b *= 3;
-c *= 10;
+a /= 12;
+b /= 4;
+c /= 11;
