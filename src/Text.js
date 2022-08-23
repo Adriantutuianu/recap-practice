370ecaf063dbf8df5604775ffd0708be6e5b41a1
Adrian <adriantutuianu01@gmail.com>
2022-08-24 00:15:49 +0100

    Divide One Decimal by Another with JavaScript

--- src/Text.js
+++ src/Text.js
@@ -1,7 +1,5 @@
-// In JavaScript, you can also perform calculations with decimal numbers, just like whole numbers.
+// Now let's divide one decimal by another.
 
-// Let's multiply two decimals together to get their product.
+// Change the 0.0 so that quotient will equal to 2.2.
 
-// Change the 0.0 so that product will equal 5.0.
-
-const product = 2.0 * 2.5;
+const quotient = 4.4 / 2.0; // Change this line
