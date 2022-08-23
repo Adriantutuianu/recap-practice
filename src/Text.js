4cdf1d74146a49550e323867a10685e131e37dd0
Adrian <adriantutuianu01@gmail.com>
2022-08-23 23:28:25 +0100

    Initializing Variables with the Assignment Operator

--- src/Text.js
+++ src/Text.js
@@ -1,16 +1,8 @@
-// Assigning the Value of One Variable to Another
-// After a value is assigned to a variable using the assignment operator, you can assign the value of that variable to another variable using the assignment operator.
+// It is common to initialize a variable to an initial value in the same line as it is declared.
 
-// var myVar;
-// myVar = 5;
-// var myNum;
-// myNum = myVar;
-// The above declares a myVar variable with no value, then assigns it the value 5. Next, a variable named myNum is declared with no value. Then, the contents of myVar (which is 5) is assigned to the variable myNum. Now, myNum also has the value of 5.
+// var myVar = 0;
+// Creates a new variable called myVar and assigns it an initial value of 0.
 
-// Assign the contents of a to variable b.
+// Define a variable a with var and initialize it to a value of 9.
 
-// Setup
-var a;
-a = 7;
-var b;
-b = a;
+var a = 9;
