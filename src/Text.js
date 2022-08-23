195bc00ad1388024a22ad9e7df4551827dc93510
Adrian <adriantutuianu01@gmail.com>
2022-08-23 23:32:32 +0100

    Understanding Uninitialized Variables

--- src/Text.js
+++ src/Text.js
@@ -1,12 +1,13 @@
-// Previously you used the following code to declare a variable:
+// When JavaScript variables are declared, they have an initial value of undefined. If you do a mathematical operation on an undefined variable your result will be NaN which means "Not a Number". If you concatenate a string with an undefined variable, you will get a string of undefined.
 
-// var myName;
-// But you can also declare a string variable like this:
+// Initialize the three variables a, b, and c with 5, 10, and "I am a" respectively so that they will not be undefined.
 
-// var myName = "your name";
-// "your name" is called a string literal. A string literal, or string, is a series of zero or more characters enclosed in single or double quotes.
+// Only change code below this line
+var a = 5;
+var b = 10;
+var c = "I am a";
+// Only change code above this line
 
-// Create two new string variables: myFirstName and myLastName and assign them the values of your first and last name, respectively.
-
-var myFirstName = "Adrian";
-var myLastName = "Tutuianu";
+a = a + 1;
+b = b + 5;
+c = c + " String!";
