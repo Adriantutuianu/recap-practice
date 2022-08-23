03f1f5cc4b47698180126e70baca57d3928c7b28
Adrian <adriantutuianu01@gmail.com>
2022-08-23 23:25:04 +0100

    Storing-values-with-the-assignment-operator

--- src/Text.js
+++ src/Text.js
@@ -1,15 +1,19 @@
-// Declare JavaScript Variables
-// In computer science, data is anything that is meaningful to the computer. JavaScript provides eight different data types which are undefined, null, boolean, string, symbol, bigint, number, and object.
+// Storing Values with the Assignment Operator
+// In JavaScript, you can store a value in a variable with the assignment operator (=).
 
-// For example, computers distinguish between numbers, such as the number 12, and strings, such as "12", "dog", or "123 cats", which are collections of characters. Computers can perform mathematical operations on a number, but not on a string.
+// myVariable = 5;
+// This assigns the Number value 5 to myVariable.
 
-// Variables allow computers to store and manipulate data in a dynamic fashion. They do this by using a "label" to point to the data rather than using the data itself. Any of the eight data types may be stored in a variable.
+// If there are any calculations to the right of the = operator, those are performed before the value is assigned to the variable on the left of the operator.
 
-// Variables are similar to the x and y variables you use in mathematics, which means they're a simple name to represent the data we want to refer to. Computer variables differ from mathematical variables in that they can store different values at different times.
+// var myVar;
+// myVar = 5;
+// First, this code creates a variable named myVar. Then, the code assigns 5 to myVar. Now, if myVar appears again in the code, the program will treat it as if it is 5.
 
-// We tell JavaScript to create or declare a variable by putting the keyword var in front of it, like so:
+// Assign the value 7 to variable a.
 
-// var ourName;
-// creates a variable called ourName. In JavaScript we end statements with semicolons. Variable names can be made up of numbers, letters, and $ or _, but may not contain spaces or start with a number.
+// Setup
+var a;
 
-var myName;
+// Only change code below this line
+a = 7;
