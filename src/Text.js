386fc698430c7f5fd216e07c03ead15087a4bf65
Adrian <adriantutuianu01@gmail.com>
2022-08-24 23:25:00 +0100

    Constructing Strings with Variables

--- src/Text.js
+++ src/Text.js
@@ -1,14 +1,13 @@
-// We can also use the += operator to concatenate a string onto the end of an existing string variable. This can be very helpful to break a long string over several lines.
-
-// Note: Watch out for spaces. Concatenation does not add spaces between concatenated strings, so you'll need to add them yourself.
+// Sometimes you will need to build a string. By using the concatenation operator (+), you can insert one or more variables into a string you're building.
 
 // Example:
 
-// let ourStr = "I come first. ";
-// ourStr += "I come second.";
-// ourStr now has a value of the string I come first. I come second..
+// const ourName = "freeCodeCamp";
+// const ourStr = "Hello, our name is " + ourName + ", how are you?";
+// ourStr would have a value of the string Hello, our name is freeCodeCamp, how are you?.
 
-// Build myStr over several lines by concatenating these two strings: This is the first sentence. and This is the second sentence. using the += operator. Use the += operator similar to how it is shown in the example and be sure to include a space between the two strings. Start by assigning the first string to myStr, then add on the second string.
+// Set myName to a string equal to your name and build myStr with myName between the strings My name is and and I am well!
 
-var myStr = "This is the first sentence.";
-myStr += " This is the second sentence.";
+// Only change code below this line
+const myName = "Adrian";
+const myStr = "My name is " + myName + "and I am well!";
