d097672e39517a79c6a4810b57e3faa914a3e717
Adrian <adriantutuianu01@gmail.com>
2022-08-24 23:10:14 +0100

    Escape Sequences in Strings

--- src/Text.js
+++ src/Text.js
@@ -1,22 +1,27 @@
-// String values in JavaScript may be written with single or double quotes, as long as you start and end with the same type of quote. Unlike some other programming languages, single and double quotes work the same in JavaScript.
+// Quotes are not the only characters that can be escaped inside a string. There are two reasons to use escaping characters:
 
-// const doubleQuoteStr = "This is a string";
-// const singleQuoteStr = 'This is also a string';
-// The reason why you might want to use one type of quote over the other is if you want to use both in a string. This might happen if you want to save a conversation in a string and have the conversation in quotes. Another use for it would be saving an <a> tag with various attributes in quotes, all within a string.
+// To allow you to use characters you may not otherwise be able to type out, such as a carriage return.
+// To allow you to represent multiple quotes in a string without JavaScript misinterpreting what you mean.
+// We learned this in the previous challenge.
 
-// const conversation = 'Finn exclaims to Jake, "Algebraic!"';
-// However, this becomes a problem if you need to use the outermost quotes within it. Remember, a string has the same kind of quote at the beginning and end. But if you have that same quote somewhere in the middle, the string will stop early and throw an error.
+// Code	Output
+// \'	single quote
+// \"	double quote
+// \\	backslash
+// \n	newline
+// \r	carriage return
+// \t	tab
+// \b	word boundary
+// \f	form feed
+// Note that the backslash itself must be escaped in order to display as a backslash.
 
-// const goodStr = 'Jake asks Finn, "Hey, let\'s go on an adventure?"';
-// const badStr = 'Finn responds, "Let's go!"';
-// Here badStr will throw an error.
+// Assign the following three lines of text into the single variable myStr using escape sequences.
 
-// In the goodStr above, you can use both quotes safely by using the backslash \ as an escape character.
+// FirstLine
+//     \SecondLine
+// ThirdLine
+// You will need to use escape sequences to insert special characters correctly. You will also need to follow the spacing as it looks above, with no spaces between escape sequences or words.
 
-// Note: The backslash \ should not be confused with the forward slash /. They do not do the same thing.
+// Note: The indentation for SecondLine is achieved with the tab escape character, not spaces.
 
-// Change the provided string to a string with single quotes at the beginning and end and no escape characters.
-
-// Right now, the <a> tag in the string uses double quotes everywhere. You will need to change the outer quotes to single quotes so you can remove the escape characters.
-
-const myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+const myStr = "FirstLine\n\t\\SecondLine\nThirdLine"; // Change this line
