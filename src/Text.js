98a38fe10e417127125e367c43085a43f9d346d9
Adrian <adriantutuianu01@gmail.com>
2022-08-25 00:08:27 +0100

    Modify Array Data With Indexes

--- src/Text.js
+++ src/Text.js
@@ -1,15 +1,17 @@
-// We can access the data inside arrays using indexes.
-
-// Array indexes are written in the same bracket notation that strings use, except that instead of specifying a character, they are specifying an entry in the array. Like strings, arrays use zero-based indexing, so the first element in an array has an index of 0.
+// Unlike strings, the entries of arrays are mutable and can be changed freely, even if the array was declared with const.
 
 // Example
 
-// const array = [50, 60, 70];
-// console.log(array[0]);
-// const data = array[1];
-// The console.log(array[0]) prints 50, and data has the value 60.
+// const ourArray = [50, 40, 30];
+// ourArray[0] = 15;
+// ourArray now has the value [15, 40, 30].
 
-// Create a variable called myData and set it to equal the first value of myArray using bracket notation.
+// Note: There shouldn't be any spaces between the array name and the square brackets, like array [0]. Although JavaScript is able to process this correctly, this may confuse other programmers reading your code.
 
-const myArray = [50, 60, 70];
-const myData = myArray[0];
+// Modify the data stored at index 0 of myArray to a value of 45.
+
+// Setup
+const myArray = [18, 64, 99];
+
+// Only change code below this line
+myArray[0] = 45;
