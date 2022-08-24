38f90275921aeaf68c673589c9c11169a9b9f3d8
Adrian <adriantutuianu01@gmail.com>
2022-08-25 00:06:56 +0100

    Access Array Data with Indexes

--- src/Text.js
+++ src/Text.js
@@ -1,12 +1,15 @@
-// You can also nest arrays within other arrays, like below:
+// We can access the data inside arrays using indexes.
 
-// const teams = [["Bulls", 23], ["White Sox", 45]];
-// This is also called a multi-dimensional array.
+// Array indexes are written in the same bracket notation that strings use, except that instead of specifying a character, they are specifying an entry in the array. Like strings, arrays use zero-based indexing, so the first element in an array has an index of 0.
 
-// Create a nested array called myArray.
+// Example
 
-// Only change code below this line
-const myArray = [
-  ["Adi", 4],
-  ["Bye", 1],
-];
+// const array = [50, 60, 70];
+// console.log(array[0]);
+// const data = array[1];
+// The console.log(array[0]) prints 50, and data has the value 60.
+
+// Create a variable called myData and set it to equal the first value of myArray using bracket notation.
+
+const myArray = [50, 60, 70];
+const myData = myArray[0];
