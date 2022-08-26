f0e12b0d105d3389932fe3318dc0d9145f8d4a1f
Adrian <adriantutuianu01@gmail.com>
2022-08-27 00:05:03 +0100

    Practice comparing different values

--- src/Text.js
+++ src/Text.js
@@ -1,22 +1,26 @@
-// Strict equality (===) is the counterpart to the equality operator (==). However, unlike the equality operator, which attempts to convert both values being compared to a common type, the strict equality operator does not perform a type conversion.
+// In the last two challenges, we learned about the equality operator (==) and the strict equality operator (===). Let's do a quick review and practice using these operators some more.
 
-// If the values being compared have different types, they are considered unequal, and the strict equality operator will return false.
+// If the values being compared are not of the same type, the equality operator will perform a type conversion, and then evaluate the values. However, the strict equality operator will compare both the data type and value as-is, without converting one type to the other.
 
 // Examples
 
-// 3 ===  3  // true
-// 3 === '3' // false
-// In the second example, 3 is a Number type and '3' is a String type.
+// 3 == '3' returns true because JavaScript performs type conversion from string to number. 3 === '3' returns false because the types are different and type conversion is not performed.
 
-// Use the strict equality operator in the if statement so the function will return the string Equal when val is strictly equal to 7.
+// Note: In JavaScript, you can determine the type of a variable or a value with the typeof operator, as follows:
+
+// typeof 3
+// typeof '3'
+// typeof 3 returns the string number, and typeof '3' returns the string string.
+
+// The compareEquality function in the editor compares two values using the equality operator. Modify the function so that it returns the string Equal only when the values are strictly equal.
 
 // Setup
-function testStrict(val) {
-  if (val === 7) {
+function compareEquality(a, b) {
+  if (a === b) {
     // Change this line
     return "Equal";
   }
   return "Not Equal";
 }
 
-testStrict(10);
+compareEquality(10, "10");
