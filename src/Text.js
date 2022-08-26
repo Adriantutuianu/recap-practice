df8fcf7181bf66dea9a783acbabaae859c77a8b7
Adrian <adriantutuianu01@gmail.com>
2022-08-27 00:22:30 +0100

    Logical Order in If Else Statements

--- src/Text.js
+++ src/Text.js
@@ -1,22 +1,47 @@
-// If you have multiple conditions that need to be addressed, you can chain if statements together with else if statements.
+// Order is important in if, else if statements.
 
-// if (num > 15) {
-//   return "Bigger than 15";
-// } else if (num < 5) {
-//   return "Smaller than 5";
-// } else {
-//   return "Between 5 and 15";
+// The function is executed from top to bottom so you will want to be careful of what statement comes first.
+
+// Take these two functions as an example.
+
+// Here's the first:
+
+// function foo(x) {
+//   if (x < 1) {
+//     return "Less than one";
+//   } else if (x < 2) {
+//     return "Less than two";
+//   } else {
+//     return "Greater than or equal to two";
+//   }
 // }
-// Convert the logic to use else if statements.
+// And the second just switches the order of the statements:
 
-function testElseIf(val) {
-  if (val > 10) {
-    return "Greater than 10";
-  } else if (val < 5) {
-    return "Smaller than 5";
+// function bar(x) {
+//   if (x < 2) {
+//     return "Less than two";
+//   } else if (x < 1) {
+//     return "Less than one";
+//   } else {
+//     return "Greater than or equal to two";
+//   }
+// }
+// While these two functions look nearly identical if we pass a number to both we get different outputs.
+
+// foo(0)
+// bar(0)
+// foo(0) will return the string Less than one, and bar(0) will return the string Less than two.
+
+// Change the order of logic in the function so that it will return the correct statements in all cases.
+
+function orderMyLogic(val) {
+  if (val < 5) {
+    return "Less than 5";
+  } else if (val < 10) {
+    return "Less than 10";
   } else {
-    return "Between 5 and 10";
+    return "Greater than or equal to 10";
   }
 }
 
-testElseIf(7);
+orderMyLogic(7);
