0f9114cc7f93218f483341d48f9bf2225f342694
Adrian <adriantutuianu01@gmail.com>
2022-08-27 00:02:01 +0100

    Comparison with the Equality Operator

--- src/Text.js
+++ src/Text.js
@@ -1,34 +1,28 @@
-// if statements are used to make decisions in code. The keyword if tells JavaScript to execute the code in the curly braces under certain conditions, defined in the parentheses. These conditions are known as Boolean conditions and they may only be true or false.
+// There are many comparison operators in JavaScript. All of these operators return a boolean true or false value.
 
-// When the condition evaluates to true, the program executes the statement inside the curly braces. When the Boolean condition evaluates to false, the statement inside the curly braces will not execute.
+// The most basic operator is the equality operator ==. The equality operator compares two values and returns true if they're equivalent or false if they are not. Note that equality is different from assignment (=), which assigns the value on the right of the operator to a variable on the left.
 
-// Pseudocode
-
-// if (condition is true) {
-//   statement is executed
-// }
-// Example
-
-// function test (myCondition) {
-//   if (myCondition) {
-//     return "It was true";
+// function equalityTest(myVal) {
+//   if (myVal == 10) {
+//     return "Equal";
 //   }
-//   return "It was false";
+//   return "Not Equal";
 // }
+// If myVal is equal to 10, the equality operator returns true, so the code in the curly braces will execute, and the function will return Equal. Otherwise, the function will return Not Equal. In order for JavaScript to compare two different data types (for example, numbers and strings), it must convert one type to another. This is known as Type Coercion. Once it does, however, it can compare terms as follows:
 
-// test(true);
-// test(false);
-// test(true) returns the string It was true, and test(false) returns the string It was false.
+// 1   ==  1  // true
+// 1   ==  2  // false
+// 1   == '1' // true
+// "3" ==  3  // true
+// Add the equality operator to the indicated line so that the function will return the string Equal when val is equivalent to 12.
 
-// When test is called with a value of true, the if statement evaluates myCondition to see if it is true or not. Since it is true, the function returns It was true. When we call test with a value of false, myCondition is not true and the statement in the curly braces is not executed and the function returns It was false.
-
-// Create an if statement inside the function to return Yes, that was true if the parameter wasThatTrue is true and return No, that was false otherwise.
-
-function trueOrFalse(wasThatTrue) {
-  // Only change code below this line
-  if (wasThatTrue) {
-    return "Yes, that was true";
+// Setup
+function testEqual(val) {
+  if (val == 12) {
+    // Change this line
+    return "Equal";
   }
-  return "No, that was false";
-  // Only change code above this line
+  return "Not Equal";
 }
+
+testEqual(10);
