1c98484a30ef33e48691ad25b725e40223a119d7
Adrian <adriantutuianu01@gmail.com>
2022-08-27 00:47:09 +0100

    Golf Code

--- src/Text.js
+++ src/Text.js
@@ -1,38 +1,45 @@
-// if/else statements can be chained together for complex logic. Here is pseudocode of multiple chained if / else if statements:
+// In the game of Golf, each hole has a par, meaning, the average number of strokes a golfer is expected to make in order to sink the ball in the hole to complete the play. Depending on how far above or below par your strokes are, there is a different nickname.
 
-// if (condition1) {
-//   statement1
-// } else if (condition2) {
-//   statement2
-// } else if (condition3) {
-//   statement3
-// . . .
-// } else {
-//   statementN
-// }
-// Write chained if/else if statements to fulfill the following conditions:
+// Your function will be passed par and strokes arguments. Return the correct string according to this table which lists the strokes in order of priority; top (highest) to bottom (lowest):
 
-// num < 5 - return Tiny
-// num < 10 - return Small
-// num < 15 - return Medium
-// num < 20 - return Large
-// num >= 20 - return Huge
+// Strokes	Return
+// 1	"Hole-in-one!"
+// <= par - 2	"Eagle"
+// par - 1	"Birdie"
+// par	"Par"
+// par + 1	"Bogey"
+// par + 2	"Double Bogey"
+// >= par + 3	"Go Home!"
+// par and strokes will always be numeric and positive. We have added an array of all the names for your convenience.
 
-function testSize(num) {
+const names = [
+  "Hole-in-one!",
+  "Eagle",
+  "Birdie",
+  "Par",
+  "Bogey",
+  "Double Bogey",
+  "Go Home!",
+];
+
+function golfScore(par, strokes) {
   // Only change code below this line
-  if (num < 5) {
-    return "Tiny";
-  } else if (num < 10) {
-    return "Small";
-  } else if (num < 15) {
-    return "Medium";
-  } else if (num < 20) {
-    return "Large";
-  } else if (num >= 20) {
-    return "Huge";
+  if (strokes == 1) {
+    return names[0];
+  } else if (strokes <= par - 2) {
+    return names[1];
+  } else if (strokes === par - 1) {
+    return names[2];
+  } else if (strokes === par) {
+    return names[3];
+  } else if (strokes === par + 1) {
+    return names[4];
+  } else if (strokes === par + 2) {
+    return names[5];
+  } else {
+    return names[6];
   }
-
   // Only change code above this line
 }
 
-testSize(7);
+golfScore(5, 4);
