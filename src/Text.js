05fc7cf19c0be7469a4221d5a9336ac4daaed1fa
Adrian <adriantutuianu01@gmail.com>
2022-08-27 00:39:14 +0100

    Chaining If Else Statements

--- src/Text.js
+++ src/Text.js
@@ -1,47 +1,38 @@
-// Order is important in if, else if statements.
+// if/else statements can be chained together for complex logic. Here is pseudocode of multiple chained if / else if statements:
 
-// The function is executed from top to bottom so you will want to be careful of what statement comes first.
-
-// Take these two functions as an example.
-
-// Here's the first:
-
-// function foo(x) {
-//   if (x < 1) {
-//     return "Less than one";
-//   } else if (x < 2) {
-//     return "Less than two";
-//   } else {
-//     return "Greater than or equal to two";
-//   }
+// if (condition1) {
+//   statement1
+// } else if (condition2) {
+//   statement2
+// } else if (condition3) {
+//   statement3
+// . . .
+// } else {
+//   statementN
 // }
-// And the second just switches the order of the statements:
+// Write chained if/else if statements to fulfill the following conditions:
 
-// function bar(x) {
-//   if (x < 2) {
-//     return "Less than two";
-//   } else if (x < 1) {
-//     return "Less than one";
-//   } else {
-//     return "Greater than or equal to two";
-//   }
-// }
-// While these two functions look nearly identical if we pass a number to both we get different outputs.
+// num < 5 - return Tiny
+// num < 10 - return Small
+// num < 15 - return Medium
+// num < 20 - return Large
+// num >= 20 - return Huge
 
-// foo(0)
-// bar(0)
-// foo(0) will return the string Less than one, and bar(0) will return the string Less than two.
-
-// Change the order of logic in the function so that it will return the correct statements in all cases.
-
-function orderMyLogic(val) {
-  if (val < 5) {
-    return "Less than 5";
-  } else if (val < 10) {
-    return "Less than 10";
-  } else {
-    return "Greater than or equal to 10";
+function testSize(num) {
+  // Only change code below this line
+  if (num < 5) {
+    return "Tiny";
+  } else if (num < 10) {
+    return "Small";
+  } else if (num < 15) {
+    return "Medium";
+  } else if (num < 20) {
+    return "Large";
+  } else if (num >= 20) {
+    return "Huge";
   }
+
+  // Only change code above this line
 }
 
-orderMyLogic(7);
+testSize(7);
