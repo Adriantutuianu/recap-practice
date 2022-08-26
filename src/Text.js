dcb477d9c9d53f4f1ebdc7421263da3e6a892795
Adrian <adriantutuianu01@gmail.com>
2022-08-27 00:03:10 +0100

    Comparison with the Strict Equality Operator

--- src/Text.js
+++ src/Text.js
@@ -1,28 +1,22 @@
-// There are many comparison operators in JavaScript. All of these operators return a boolean true or false value.
+// Strict equality (===) is the counterpart to the equality operator (==). However, unlike the equality operator, which attempts to convert both values being compared to a common type, the strict equality operator does not perform a type conversion.
 
-// The most basic operator is the equality operator ==. The equality operator compares two values and returns true if they're equivalent or false if they are not. Note that equality is different from assignment (=), which assigns the value on the right of the operator to a variable on the left.
+// If the values being compared have different types, they are considered unequal, and the strict equality operator will return false.
 
-// function equalityTest(myVal) {
-//   if (myVal == 10) {
-//     return "Equal";
-//   }
-//   return "Not Equal";
-// }
-// If myVal is equal to 10, the equality operator returns true, so the code in the curly braces will execute, and the function will return Equal. Otherwise, the function will return Not Equal. In order for JavaScript to compare two different data types (for example, numbers and strings), it must convert one type to another. This is known as Type Coercion. Once it does, however, it can compare terms as follows:
+// Examples
 
-// 1   ==  1  // true
-// 1   ==  2  // false
-// 1   == '1' // true
-// "3" ==  3  // true
-// Add the equality operator to the indicated line so that the function will return the string Equal when val is equivalent to 12.
+// 3 ===  3  // true
+// 3 === '3' // false
+// In the second example, 3 is a Number type and '3' is a String type.
+
+// Use the strict equality operator in the if statement so the function will return the string Equal when val is strictly equal to 7.
 
 // Setup
-function testEqual(val) {
-  if (val == 12) {
+function testStrict(val) {
+  if (val === 7) {
     // Change this line
     return "Equal";
   }
   return "Not Equal";
 }
 
-testEqual(10);
+testStrict(10);
