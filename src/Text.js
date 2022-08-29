0aae734de492f327b1220c62a706d3b614acebda
Adrian <adriantutuianu01@gmail.com>
2022-08-29 22:02:59 +0100

    Returning Boolean Values from Functions

--- src/Text.js
+++ src/Text.js
@@ -1,48 +1,25 @@
-// If you have many options to choose from, a switch statement can be easier to write than many chained if/else if statements. The following:
+// You may recall from Comparison with the Equality Operator that all comparison operators return a boolean true or false value.
 
-// if (val === 1) {
-//   answer = "a";
-// } else if (val === 2) {
-//   answer = "b";
-// } else {
-//   answer = "c";
+// Sometimes people use an if/else statement to do a comparison, like this:
+
+// function isEqual(a, b) {
+//   if (a === b) {
+//     return true;
+//   } else {
+//     return false;
+//   }
 // }
-// can be replaced with:
+// But there's a better way to do this. Since === returns true or false, we can return the result of the comparison:
 
-// switch (val) {
-//   case 1:
-//     answer = "a";
-//     break;
-//   case 2:
-//     answer = "b";
-//     break;
-//   default:
-//     answer = "c";
+// function isEqual(a, b) {
+//   return a === b;
 // }
-// Change the chained if/else if statements into a switch statement.
+// Fix the function isLess to remove the if/else statements.
 
-function chainToSwitch(val) {
-  let answer = "";
+function isLess(a, b) {
   // Only change code below this line
-  switch (val) {
-    case "bob":
-      answer = "Marley";
-      break;
-    case 42:
-      answer = "The Answer";
-      break;
-    case 1:
-      answer = "There is no #1";
-      break;
-    case 99:
-      answer = "Missed me by this much!";
-      break;
-    case 7:
-      answer = "Ate Nine";
-      break;
-  }
+  return a < b;
   // Only change code above this line
-  return answer;
 }
 
-chainToSwitch(7);
+isLess(10, 15);
