c30a56f66e67c60e22e54bf08e1cd38e1d8e2841
Adrian <adriantutuianu01@gmail.com>
2022-08-29 22:13:38 +0100

    Counting Cards

--- src/Text.js
+++ src/Text.js
@@ -1,30 +1,52 @@
-// When a return statement is reached, the execution of the current function stops and control returns to the calling location.
+// In the casino game Blackjack, a player can determine whether they have an advantage on the next hand over the house by keeping track of the relative number of high and low cards remaining in the deck. This is called Card Counting.
 
-// Example
+// Having more high cards remaining in the deck favors the player. Each card is assigned a value according to the table below. When the count is positive, the player should bet high. When the count is zero or negative, the player should bet low.
 
-// function myFun() {
-//   console.log("Hello");
-//   return "World";
-//   console.log("byebye")
-// }
-// myFun();
-// The above will display the string Hello in the console, and return the string World. The string byebye will never display in the console, because the function exits at the return statement.
+// Count Change	Cards
+// +1	2, 3, 4, 5, 6
+// 0	7, 8, 9
+// -1	10, 'J', 'Q', 'K', 'A'
+// You will write a card counting function. It will receive a card parameter, which can be a number or a string, and increment or decrement the global count variable according to the card's value (see table). The function will then return a string with the current count and the string Bet if the count is positive, or Hold if the count is zero or negative. The current count and the player's decision (Bet or Hold) should be separated by a single space.
 
-// Modify the function abTest so that if a or b are less than 0 the function will immediately exit with a value of undefined.
+// Example Outputs: -3 Hold or 5 Bet
 
 // Hint
-// Remember that undefined is a keyword, not a string.
+// Do NOT reset count to 0 when value is 7, 8, or 9.
+// Do NOT return an array.
+// Do NOT include quotes (single or double) in the output.
 
-// Setup
-function abTest(a, b) {
+let count = 0;
+
+function cc(card) {
   // Only change code below this line
-  if (a < 0 || b < 0) {
-    return undefined;
+
+  switch (card) {
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+      count++;
+      break;
+    case 10:
+    case "J":
+    case "Q":
+    case "K":
+    case "A":
+      count--;
+      break;
   }
-
+  if (count > 0) {
+    return count + " Bet";
+  } else {
+    return count + " Hold";
+  }
+  return "Change Me";
   // Only change code above this line
-
-  return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
 }
 
-abTest(2, 2);
+cc(2);
+cc(3);
+cc(7);
+cc("K");
+cc("A");
