144e990cc67e12b7a3fe6c3310fd3aa9cc35ce23
Adrian <adriantutuianu01@gmail.com>
2022-08-30 23:41:13 +0100

    Profile Lookup

--- src/Text.js
+++ src/Text.js
@@ -1,31 +1,56 @@
-// Recursion is the concept that a function can be expressed in terms of itself. To help understand this, start by thinking about the following task: multiply the first n elements of an array to create the product of those elements. Using a for loop, you could do this:
+// We have an array of objects representing different people in our contacts lists.
 
-//   function multiply(arr, n) {
-//     let product = 1;
-//     for (let i = 0; i < n; i++) {
-//       product *= arr[i];
-//     }
-//     return product;
-//   }
-// However, notice that multiply(arr, n) == multiply(arr, n - 1) * arr[n - 1]. That means you can rewrite multiply in terms of itself and never need to use a loop.
+// A lookUpProfile function that takes name and a property (prop) as arguments has been pre-written for you.
 
-//   function multiply(arr, n) {
-//     if (n <= 0) {
-//       return 1;
-//     } else {
-//       return multiply(arr, n - 1) * arr[n - 1];
-//     }
-//   }
-// The recursive version of multiply breaks down like this. In the base case, where n <= 0, it returns 1. For larger values of n, it calls itself, but with n - 1. That function call is evaluated in the same way, calling multiply again until n <= 0. At this point, all the functions can return and the original multiply returns the answer.
+// The function should check if name is an actual contact's firstName and the given property (prop) is a property of that contact.
 
-// Note: Recursive functions must have a base case when they return without calling the function again (in this example, when n <= 0), otherwise they can never finish executing.
+// If both are true, then return the "value" of that property.
 
-// // Write a recursive function, sum(arr, n), that returns the sum of the first n elements of an array arr.
+// If name does not correspond to any contacts then return the string No such contact.
 
-function sum(arr, n) {
-  if (n <= 0) {
-    return 0;
-  } else {
-    return sum(arr, n - 1) + arr[n - 1];
+// If prop does not correspond to any valid properties of a contact found to match name then return the string No such property.
+
+// Setup
+const contacts = [
+  {
+    firstName: "Akira",
+    lastName: "Laine",
+    number: "0543236543",
+    likes: ["Pizza", "Coding", "Brownie Points"],
+  },
+  {
+    firstName: "Harry",
+    lastName: "Potter",
+    number: "0994372684",
+    likes: ["Hogwarts", "Magic", "Hagrid"],
+  },
+  {
+    firstName: "Sherlock",
+    lastName: "Holmes",
+    number: "0487345643",
+    likes: ["Intriguing Cases", "Violin"],
+  },
+  {
+    firstName: "Kristian",
+    lastName: "Vos",
+    number: "unknown",
+    likes: ["JavaScript", "Gaming", "Foxes"],
+  },
+];
+
+// Only change code below this line
+function lookUpProfile(name, prop) {
+  for (let x = 0; x < contacts.length; x++) {
+    if (contacts[x].firstName === name) {
+      if (contacts[x].hasOwnProperty(prop)) {
+        return contacts[x][prop];
+      } else {
+        return "No such property";
+      }
+    }
   }
+  return "No such contact";
 }
+// Only change code above this line
+
+lookUpProfile("Akira", "likes");
