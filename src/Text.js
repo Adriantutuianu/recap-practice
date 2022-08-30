39090db3b01d867f774f33a7cf6be161d78bce80
Adrian <adriantutuianu01@gmail.com>
2022-08-30 23:53:41 +0100

    Generate Random Whole Numbers within a Range

--- src/Text.js
+++ src/Text.js
@@ -1,19 +1,14 @@
-// It's great that we can generate random decimal numbers, but it's even more useful if we use it to generate random whole numbers.
+// Instead of generating a random whole number between zero and a given number like we did before, we can generate a random whole number that falls within a range of two specific numbers.
 
-// Use Math.random() to generate a random decimal.
-// Multiply that random decimal by 20.
-// Use another function, Math.floor() to round the number down to its nearest whole number.
-// Remember that Math.random() can never quite return a 1 and, because we're rounding down, it's impossible to actually get 20. This technique will give us a whole number between 0 and 19.
+// To do this, we'll define a minimum number min and a maximum number max.
 
-// Putting everything together, this is what our code looks like:
+// Here's the formula we'll use. Take a moment to read it and try to understand what this code is doing:
 
-// Math.floor(Math.random() * 20);
-// We are calling Math.random(), multiplying the result by 20, then passing the value to Math.floor() function to round the value down to the nearest whole number.
+// Math.floor(Math.random() * (max - min + 1)) + min
+// Create a function called randomRange that takes a range myMin and myMax and returns a random whole number that's greater than or equal to myMin, and is less than or equal to myMax, inclusive.
 
-// Use this technique to generate and return a random whole number between 0 and 9.
-
-function randomWholeNum() {
+function randomRange(myMin, myMax) {
   // Only change code below this line
-
-  return Math.floor(Math.random() * 10);
+  return Math.floor(Math.random() * (myMax - myMin + 1) + myMin);
+  // Only change code above this line
 }
