2b6d08ff769c0cf560a20a271ef7f3ef57f8c3d1
Adrian <adriantutuianu01@gmail.com>
2022-08-31 22:47:06 +0100

    Catch Mixed Usage of Single and Double Quotes

--- src/Text.js
+++ src/Text.js
@@ -1,9 +1,18 @@
-// Another syntax error to be aware of is that all opening parentheses, brackets, curly braces, and quotes have a closing pair. Forgetting a piece tends to happen when you're editing existing code and inserting items with one of the pair types. Also, take care when nesting code blocks into others, such as adding a callback function as an argument to a method.
+// JavaScript allows the use of both single (') and double (") quotes to declare a string. Deciding which one to use generally comes down to personal preference, with some exceptions.
 
-// One way to avoid this mistake is as soon as the opening character is typed, immediately include the closing match, then move the cursor back between them and continue coding. Fortunately, most modern code editors generate the second half of the pair automatically.
+// Having two choices is great when a string has contractions or another piece of text that's in quotes. Just be careful that you don't close the string too early, which causes a syntax error.
 
-// Fix the two pair errors in the code.
+// Here are some examples of mixing quotes:
 
-let myArray = [1, 2, 3];
-let arraySum = myArray.reduce((previous, current) => previous + current);
-console.log(`Sum of array values is: ${arraySum}`);
+// const grouchoContraction = "I've had a perfectly wonderful evening, but this wasn't it.";
+// const quoteInString = "Groucho Marx once said 'Quote me as saying I was mis-quoted.'";
+// const uhOhGroucho = 'I've had a perfectly wonderful evening, but this wasn't it.';
+// The first two are correct, but the third is incorrect.
+
+// Of course, it is okay to use only one style of quotes. You can escape the quotes inside the string by using the backslash (\) escape character:
+
+// const allSameQuotes = 'I\'ve had a perfectly wonderful evening, but this wasn\'t it.';
+// Fix the string so it either uses different quotes for the href value, or escape the existing ones. Keep the double quote marks around the entire string.
+
+let innerHtml = "<p>Click here to <a href='#Home'>return home</a></p>";
+console.log(innerHtml);
