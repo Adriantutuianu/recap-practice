cf03d5db7b7ac0f5fca4ecd10c810504c23607cd
Adrian <adriantutuianu01@gmail.com>
2022-08-31 22:55:40 +0100

    Catch Missing Open and Closing Parenthesis After a Function Call

--- src/Text.js
+++ src/Text.js
@@ -1,32 +1,21 @@
-// Branching programs, i.e. ones that do different things if certain conditions are met, rely on if, else if, and else statements in JavaScript. The condition sometimes takes the form of testing whether a result is equal to a value.
+// When a function or method doesn't take any arguments, you may forget to include the (empty) opening and closing parentheses when calling it. Often times the result of a function call is saved in a variable for other use in your code. This error can be detected by logging variable values (or their types) to the console and seeing that one is set to a function reference, instead of the expected value the function returns.
 
-// This logic is spoken (in English, at least) as "if x equals y, then ..." which can literally translate into code using the =, or assignment operator. This leads to unexpected control flow in your program.
+// The variables in the following example are different:
 
-// As covered in previous challenges, the assignment operator (=) in JavaScript assigns a value to a variable name. And the == and === operators check for equality (the triple === tests for strict equality, meaning both value and type are the same).
-
-// The code below assigns x to be 2, which evaluates as true. Almost every value on its own in JavaScript evaluates to true, except what are known as the "falsy" values: false, 0, "" (an empty string), NaN, undefined, and null.
-
-// let x = 1;
-// let y = 2;
-// if (x = y) {
-
-// } else {
-
+// function myFunction() {
+//   return "You rock!";
 // }
-// In this example, the code block within the if statement will run for any value of y, unless y is falsy. The else block, which we expect to run here, will not actually run.
+// let varOne = myFunction;
+// let varTwo = myFunction();
+// Here varOne is the function myFunction, and varTwo is the string You rock!.
 
-// Fix the condition so the program runs the right branch, and the appropriate value is assigned to result.
+// Fix the code so the variable result is set to the value returned from calling the function getNine.
 
-// //
-
-let x = 7;
-let y = 9;
-let result = "to come";
-
-if (x === y) {
-  result = "Equal!";
-} else {
-  result = "Not equal!";
+function getNine() {
+  let x = 6;
+  let y = 3;
+  return x + y;
 }
 
+let result = getNine();
 console.log(result);
