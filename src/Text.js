2062e27aa862430f533e5ba0d0f9d5093265abf5
Adrian <adriantutuianu01@gmail.com>
2022-08-31 22:41:13 +0100

    Catch Misspelled Variable and Function Names

--- src/Text.js
+++ src/Text.js
@@ -1,21 +1,10 @@
-// You can use typeof to check the data structure, or type, of a variable. This is useful in debugging when working with multiple data types. If you think you're adding two numbers, but one is actually a string, the results can be unexpected. Type errors can lurk in calculations or function calls. Be careful especially when you're accessing and working with external data in the form of a JavaScript Object Notation (JSON) object.
+// The console.log() and typeof methods are the two primary ways to check intermediate values and types of program output. Now it's time to get into the common forms that bugs take. One syntax-level issue that fast typers can commiserate with is the humble spelling error.
 
-// Here are some examples using typeof:
+// Transposed, missing, or mis-capitalized characters in a variable or function name will have the browser looking for an object that doesn't exist - and complain in the form of a reference error. JavaScript variable and function names are case-sensitive.
 
-// console.log(typeof "");
-// console.log(typeof 0);
-// console.log(typeof []);
-// console.log(typeof {});
-// In order, the console will display the strings string, number, object, and object.
+// Fix the two spelling errors in the code so the netWorkingCapital calculation works.
 
-// JavaScript recognizes seven primitive (immutable) data types: Boolean, Null, Undefined, Number, String, Symbol (new with ES6), and BigInt (new with ES2020), and one type for mutable items: Object. Note that in JavaScript, arrays are technically a type of object.
-
-// Add two console.log() statements to check the typeof each of the two variables seven and three in the code.
-
-let seven = 7;
-let three = "3";
-console.log(seven + three);
-// Only change code below this line
-
-console.log(typeof seven);
-console.log(typeof three);
+let receivables = 10;
+let payables = 8;
+let netWorkingCapital = receivables - payables;
+console.log(`Net working capital is: ${netWorkingCapital}`);
