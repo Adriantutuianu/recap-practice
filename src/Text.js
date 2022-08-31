92d44b56f3dae158481b2b6813f5d9ebf4deac72
Adrian <adriantutuianu01@gmail.com>
2022-08-31 22:24:28 +0100

    Use the JavaScript Console to Check the Value of a Variable

--- src/Text.js
+++ src/Text.js
@@ -1,32 +1,19 @@
-// Introduction to the Debugging Challenges
-// Debugging is a valuable and (unfortunately) necessary tool for programmers. It follows the testing phase of checking if your code works as intended, and discovering it does not. Debugging is the process of finding exactly what isn't working and fixing it. After spending time creating a brilliant block of code, it is tough realizing it may have errors. These issues generally come in three forms:
+// Both Chrome and Firefox have excellent JavaScript consoles, also known as DevTools, for debugging your JavaScript.
 
-// syntax errors that prevent a program from running
-// runtime errors when code fails to execute or has unexpected behavior
-// semantic (or logical) errors when code doesn't do what it's meant to.
-// Modern code editors (and experience) can help identify syntax errors. Semantic and runtime errors are harder to find. They may cause your program to crash, make it run forever, or give incorrect output. Think of debugging as trying to understand why your code is behaving the way it is. Example of a syntax error - often detected by the code editor:
+// You can find Developer tools in your Chrome's menu or Web Console in Firefox's menu. If you're using a different browser, or a mobile phone, we strongly recommend switching to desktop Firefox or Chrome.
 
-// funtcion willNotWork(
-//   console.log("Yuck");
-// }
-// // "function" keyword is misspelled and there's a missing parenthesis
-// Here's an example of a runtime error - often detected while the program executes:
+// The console.log() method, which "prints" the output of what's within its parentheses to the console, will likely be the most helpful debugging tool. Placing it at strategic points in your code can show you the intermediate values of variables. It's good practice to have an idea of what the output should be before looking at what it is. Having check points to see the status of your calculations throughout your code will help narrow down where the problem is.
 
-// function loopy() {
-//   while (true) {
-//     console.log('Hello, world!');
-//   }
-// }
-// // Calling loopy starts an infinite loop, which may crash your browser
-// Example of a semantic error - often detected after testing code output:
+// Here's an example to print the string Hello world! to the console:
 
-// function calcAreaOfRect(w, h) {
-//   return w + h; // This should be w * h
-// }
-// let myRectArea = calcAreaOfRect(2, 3);
-// // Correct syntax and the program executes, but this gives the wrong answer
-// Debugging is frustrating, but it helps to develop (and follow) a step-by-step approach to review your code. This means checking the intermediate values and types of variables to see if they are what they should be. You can start with a simple process of elimination.
+// console.log('Hello world!');
+// Use the console.log() method to print the value of the variable a where noted in the code.
 
-// For example, if function A works and returns what it's supposed to, then function B may have the issue. Or start checking values in a block of code from the middle to try to cut the search space in half. A problem in one spot indicates a bug in the first half of the code. If not, it's likely in the second.
+let a = 5;
+let b = 1;
+a++;
+// Only change code below this line
+console.log(a);
 
-// This section will cover a couple helpful tools to find bugs, and some of the common forms they take. Fortunately, debugging is a learnable skill that just requires a little patience and practice to master.
+let sumAB = a + b;
+console.log(sumAB);
