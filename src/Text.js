7b1fb2bbd5854c4ded9d6afb6aa5935cd5018ee3
Adrian <adriantutuianu01@gmail.com>
2022-09-09 23:22:17 +0100

    Making the guest list

--- src/Text.js
+++ src/Text.js
@@ -1,26 +1,33 @@
-// Find the Useful Emails
-// The Northcoders party invitations have been sent and responses are coming in! In the interest of inclusivity, Northcoders has opted to make this a pay-what-you-want event.
+// Making the Guest List
+// We need to keep track of the party guests, but the data we currently have combines the guests first and last names into a single name. You have been asked to separate the names to make the data easier to work with.
 
-// The finance team need to keep track of how much people are willing to pay for a ticket. You have been asked to write a function which will tell the team if an email contains information about a ticket price or not.
+// The makeGuestList function takes an object with a name property whose value will be a string consisting of a first name and a last name, separated by a space. The function should return an object.
 
-// The findTicketPrices function will take a string, and should return a value of true if the string contains a number, and false if it does not.
+// The function should remove the name property, replace it with firstName and lastName properties, as shown in the examples below.
 
 // Examples:
 
-// findTicketPrices("Hello, I think I can pay 11 pounds, is that alright?");
-// // returns true
+// makeGuestList({ name: "Hannah Fry", age: 46 })
+// // should return { firstName: "Hannah", lastName: "Fry", age: 46 }
 
-// findTicketPrices("I would be willing to pay 30 for a ticket");
-// // returns true
+// makeGuestList({ name: "Paul Erdős", age: 46 })
+// // should return { firstName: "Paul", lastName: "Erdős", age: 46 }
+// Note: all other properties should remain unchanged.
 
-// findTicketPrices("Thanks for making this event pay what you can afford!");
-// // returns false
-
-function findTicketPrices(emailString) {
+function makeGuestList(person) {
   // Your code goes here...
-  if (/\d/.test(emailString)) {
-    return true;
-  } else {
-    return false;
-  }
+  const name = person.name;
+
+  const first = name.substring(0, name.indexOf(" "));
+  const second = name.substring(name.indexOf(" ") + 1);
+
+  //create a new object with all values without name
+  const newPerson = {
+    ...person,
+  };
+  delete newPerson.name;
+  newPerson.firstName = first;
+  newPerson.lastName = second;
+
+  return newPerson;
 }
