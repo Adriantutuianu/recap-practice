7b923d9cd5d258321d14e2d9464bdaa8b7068bbe
Adrian <adriantutuianu01@gmail.com>
2022-09-09 22:45:36 +0100

    Find useful emails

--- src/Text.js
+++ src/Text.js
@@ -1,32 +1,26 @@
-// Find Even Length Strings
-// This function will take an array of values, and should return an array of all the even length strings.
+// Find the Useful Emails
+// The Northcoders party invitations have been sent and responses are coming in! In the interest of inclusivity, Northcoders has opted to make this a pay-what-you-want event.
 
-// Anything that's not a string, or not a string of even length, should not be returned in the array.
+// The finance team need to keep track of how much people are willing to pay for a ticket. You have been asked to write a function which will tell the team if an email contains information about a ticket price or not.
 
-// If there are no even length strings, it should return an empty array.
+// The findTicketPrices function will take a string, and should return a value of true if the string contains a number, and false if it does not.
 
 // Examples:
 
-// findEvenLengthStrings(['hi', 'bye'])
-// // returns ['hi']
+// findTicketPrices("Hello, I think I can pay 11 pounds, is that alright?");
+// // returns true
 
-// findEvenLengthStrings(['bye'])
-// // returns []
+// findTicketPrices("I would be willing to pay 30 for a ticket");
+// // returns true
 
-function findEvenLengthStrings(items) {
+// findTicketPrices("Thanks for making this event pay what you can afford!");
+// // returns false
+
+function findTicketPrices(emailString) {
   // Your code goes here...
-  const even = [];
-
-  items.forEach((item) => {
-    if (item !== null && !Array.isArray(item) && item.length % 2 == 0) {
-      even.push(item);
-    }
-  });
-
-  console.log(even);
-  if (!even.length) {
-    return [];
+  if (/\d/.test(emailString)) {
+    return true;
   } else {
-    return even;
+    return false;
   }
 }
