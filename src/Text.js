1787a1e627ad380f6c01860f16c7a1ade460bd42
Adrian <adriantutuianu01@gmail.com>
2022-09-13 23:52:21 +0100

    Gathering Feedback

--- src/Text.js
+++ src/Text.js
@@ -1,29 +1,41 @@
-// Who are the Winners?
-// The Northcoders party is nearly over & it's time to select the winners of the prize draw raffle!
+// Gathering Feedback
+// Phew! All your hard work paid off and the event was a success! Now that you have had some time to relax, it's time to consider the guests feedback.
 
-// You have been asked to write a function which will select the winners based on two factors: how much they paid, and their seat number. If both of these numbers are odd, the attendee wins a prize!
+// You decide to break the feedback into three groups: positive (7-10), negative (0-3) or neutral (4-6).
 
-// The pickWinners function will be passed an array of numbers representing ticket costs as its only argument. The index position represents the seat number.
+// The function gatherFeedback takes an array of arrays, each of these arrays contains both a string of the name of the attendee and a number showing how they rated the party out of 10.
 
-// Your task is to return an array of objects. Each object should contain two keys. The first key is seat with a value of the odd index. The second key is ticketCost with a value of the ticket price at that index in the input array.
+// It should return an object with three properties: a key of positive with a value of the number of positive reviews; a key of negative with a value of the number of negative reviews; and, a key of neutral with a value of the number of neutral reviews.
 
-// Examples below.
+// Examples:
 
-// pickWinners([6, 7, 12, 49])
-// // should return [{seat: 1, ticketCost: 7}, {seat: 3, ticketCost: 49}]
-// pickWinners([1, 3, 5, 7, 9, 11])
-// // should return [{seat: 1, ticketCost: 3}, {seat: 3, ticketCost: 7}, {seat: 5, ticketCost: 11} ]
-// pickWinners([1, 6, 13, 8, 29, 50])
-// // should return []
+// gatherFeedback([['maddie', 10], ['jatinder', 3], ['rose', 6]]);
+// // returns {positive: 1, negative: 1, neutral:1}
 
-function pickWinners(numbers) {
+// gatherFeedback([['maddie', 10], ['jatinder', 10], ['rose', 10]]);
+// // returns {positive: 3, negative: 0, neutral:0}
+
+// gatherFeedback([['maddie', 10], ['jatinder', 10], ['rose', 1]]);
+// // returns {positive: 2, negative: 1, neutral:0}
+
+function gatherFeedback(feedbackArray) {
   // Your code goes here...
-  const numbersObject = [];
 
-  numbers.map((item, index) => {
-    if (item % 2 && index % 2) {
-      numbersObject.push({ seat: index, ticketCost: item });
+  let positive = 0;
+  let negative = 0;
+  let neutral = 0;
+
+  feedbackArray.map((item) => {
+    const number = item[1];
+
+    if (number >= 7 && number <= 10) {
+      positive = positive + 1;
+    } else if (number >= 4 && number <= 6) {
+      neutral = neutral + 1;
+    } else if (number >= 0 && number <= 3) {
+      negative = negative + 1;
     }
   });
-  return numbersObject;
+
+  return { positive: positive, negative: negative, neutral: neutral };
 }
